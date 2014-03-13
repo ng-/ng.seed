f2d09a51af1fa379b9373570966fc0163ed66510
Adam Kircher <adam.kircher@gmail.com>
2014-03-13 16:27:15 -0700

    Updating installation script

--- node.sh
+++ node.sh
@@ -3,14 +3,17 @@
 # download nave, a node version management tool
 wget https://raw.github.com/isaacs/nave/master/nave.sh
 
-# Point command line’s “nave” to run the script
-sudo ln -s $PWD/nave.sh /usr/local/bin/nave
+# install latest stable version of node & npm
+./nave use stable
 
-# give yourself permission to run the script
+#move installation script out of the way
+mv nave.sh .nave/nave.sh
+
+# Make 'nave' command work without specifying a path
+sudo ln -s $PWD/.nave/nave.sh /usr/local/bin/nave
+
+# give permission to run the pathless command
 sudo chmod 755 /usr/local/bin/nave
-
-# starts virtual environment with node & npm
-nave use
 
 # start environment on future logins as well
 echo 'nave use stable' >> ~/.bashrc
@@ -19,4 +22,4 @@ echo 'nave use stable' >> ~/.bashrc
 mkdir node_modules && cd node_modules
 
 #Show user thank you message
-echo 'ng.seed has successfully installed node & npm'
+echo 'ng.seed installed node & npm. Use "rm -R .nave" to delete'
