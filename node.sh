2c0de9ab4f7009f2a327bc8f12aa07528ccb742f
Adam Kircher <adam.kircher@gmail.com>
2014-03-13 18:16:53 -0700

    Revert back to subshell

--- node.sh
+++ node.sh
@@ -13,7 +13,10 @@ sudo ln -s $PWD/nave.sh /usr/local/bin/nave
 sudo chmod 755 /usr/local/bin/nave
 
 # starts virtual environment with node & npm
-nave usemain stable
+nave use stable
+
+# start environment on future logins as well
+echo 'nave use stable' >> ~/.bashrc
 
 # this is where the magic will happen
 cd ../ && mkdir -p node_modules && cd node_modules
