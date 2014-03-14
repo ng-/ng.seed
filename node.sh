bfce50925c9f2be746f5f21810190351083b7d36
Adam Kircher <adam.kircher@gmail.com>
2014-03-13 18:00:26 -0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # let nave do all the hard work in a hidden directory
 mkdir .nave && cd .nave
 
@@ -10,14 +12,14 @@ sudo ln -s $PWD/nave.sh /usr/local/bin/nave
 # give yourself permission to run the script
 sudo chmod 755 /usr/local/bin/nave
 
-# starts virtual environment with node & npm
-nave use stable
+# this is where the magic will happen
+cd ../ && mkdir node_modules && cd node_modules
 
 # start environment on future logins as well
 echo 'nave use stable' >> ~/.bashrc
 
-# this is where the magic will happen
-cd ../ && mkdir node_modules && cd node_modules
+# starts virtual environment with node & npm
+nave use stable
 
 #Show user thank you message
 echo 'ng.seed installed node & npm'
