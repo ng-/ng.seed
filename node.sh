515ee3ab35253358df98d83a41fadbd54c0c5521
Adam Kircher <adam.kircher@gmail.com>
2014-03-13 20:02:16 -0700

    Updated node.sh

--- node.sh
+++ node.sh
@@ -3,15 +3,17 @@
 # let nave do all the hard work in a hidden directory
 mkdir -p .nave && cd .nave
 
+# create necessary folders and take ownership
+sudo mkdir -p /usr/local/{share/man,bin,lib/node,include/node}
+
 # download nave, a node version management tool
 wget https://raw.github.com/isaacs/nave/master/nave.sh
 
 # point command line’s “nave” to run the script
 sudo ln -s $PWD/nave.sh /usr/local/bin/nave
 
-# create necessary folders and take ownership
-sudo mkdir -p /usr/local/{share/man,bin,lib/node,include/node}
-sudo chown -R $USER /usr/local/{share/man,bin,lib/node,include/node}
+# give yourself permission to run the script
+sudo chmod -R 755 /usr/local/{share/man,bin,lib/node,include/node}
 
 # starts virtual environment with node & npm
 nave usemain stable
