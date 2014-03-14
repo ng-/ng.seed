c9a28463c7c2c841483bd35ea820361cbae83067
Adam Kircher <adam.kircher@gmail.com>
2014-03-13 18:13:34 -0700

    Switch to nave usemain

--- node.sh
+++ node.sh
@@ -13,10 +13,7 @@ sudo ln -s $PWD/nave.sh /usr/local/bin/nave
 sudo chmod 755 /usr/local/bin/nave
 
 # starts virtual environment with node & npm
-nave use stable
-
-# start environment on future logins as well
-echo 'nave use stable' >> ~/.bashrc
+nave usemain stable
 
 # this is where the magic will happen
 cd ../ && mkdir -p node_modules && cd node_modules
