0a9fe68f184e01454d6e67412bce9422f717aa0c
Adam Kircher <adam.kircher@gmail.com>
2014-03-13 23:58:05 -0700

    Updated node.js install script

--- node.sh
+++ node.sh
@@ -3,9 +3,6 @@
 # let nave do all the hard work in a hidden directory
 mkdir -p .nave && cd .nave
 
-# create necessary folders and take ownership
-sudo mkdir -p /usr/local/{share/man,bin,lib/node,include/node}
-
 # download nave, a node version management tool
 wget https://raw.github.com/isaacs/nave/master/nave.sh
 
@@ -13,10 +10,10 @@ wget https://raw.github.com/isaacs/nave/master/nave.sh
 sudo ln -s $PWD/nave.sh /usr/local/bin/nave
 
 # give yourself permission to run the script
-sudo chmod -R 755 /usr/local
+sudo chmod 755 /usr/local/bin/nave
 
 # starts virtual environment with node & npm
-nave usemain stable
+sudo bash nave.sh usemain stable
 
 # this is where the magic will happen
 cd ../ && mkdir -p node_modules && cd node_modules
